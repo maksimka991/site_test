639b4a1ba10749dedbe00a9848a8ac0b8053a23d
maksimka991 <maksimka9112007@gmail.com>
2015-04-14 00:39:38 +0300

    dd

---
+++ js.js
@@ -0,0 +1,16 @@
+﻿ 
+    function testBlock() {
+ var obj = document.getElementById('switch-on')
+ var obj2 = document.getElementById('switch-off')
+ 
+ 
+ if (obj.style.display == 'none') {
+  obj.style.display = 'block'; 
+  obj2.style.display = 'none'; 
+     
+ } 
+ else { 
+  obj.style.display = 'none'; 
+  obj2.style.display = 'block'; 
+ }
+} 
